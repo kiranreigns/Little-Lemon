2b540d82da835c708fc5fa5ccb59bde7e9485e6c
Kiran <kiranreigns@hotmail.com>
2025-02-06 12:49:55 +0530

    Start of the Little Lemon Project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <h1 className="font-markazi text-h1 text-primary-green font-bold">
+      Little Lemon
+    </h1>
+  );
+}
+
+export default App;
